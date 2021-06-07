61a026a2043fca75b458b02f1884501beee32538
meunomeali <>
2021-06-07 14:26:00 +0000

    3 completo

--- haskell05.hs
+++ haskell05.hs
@@ -12,15 +12,19 @@ bmi' :: Float -> Float -> String
 bmi' peso altura = if imc <= 18.5 then "ABAIXO" else if imc < 30 then "NORMAL" else "ACIMA"
   where imc = peso / (altura^2)
 
--- 3)
 
+-- 3) validação cpf
 cpfValid :: [Int] -> Bool
-cpfValid cpf =
- let digits = take 9 cpf
-     dv1 = cpfDV digits [10,9..]
-     dv2 = cpfDV (digits ++ [dv1]) [11,10..]
-  in dv1 == cpf !! 9 && dv2 == cpf !! 10
+cpfValid cpf = dv1 == cpf !! 9 && dv2 == cpf !! 10
+  where digits = take 9 cpf
+        dv1 = cpfDV digits [10,9..]
+        dv2 = cpfDV (digits ++ [dv1]) [11,10..]
 
 cpfDV :: [Int] -> [Int] -> Int
-cpfDV digits mults = if expr < 2 then 0 else 11-expr
- where expr = (sum $ zipWith (*) digits mults) `mod` 11
+cpfDV digits mults =
+  let expr = (sum $ zipWith (*) digits mults) `mod` 11
+    in if expr < 2 then 0 else 11-expr
+
+
+-- 4)
+
